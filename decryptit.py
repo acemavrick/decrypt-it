847f3621e753b02c3899a830e02b7d9e124e02fa
acemavrick <116685696+acemavrick@users.noreply.github.com>
2023-01-07 17:38:40 -0600

    Created encryptions
    Created all encryptions
    Caesar shift left

--- decryptit.py
+++ decryptit.py
@@ -9,21 +9,26 @@ with open('quotes.txt') as qfile:
 # quotes now contains all the messages
 
 from random import *
-# cite random somehow?
+# Van Rossum, G. (2020). The Python Library Reference, release 3.8.2. Python Software Foundation.
 
 instructions = \
     '''
     Decrypt
     '''
 
+### caesar shift left
+
 # create functions for diff. encryptions
 
 # ascii shift
-def ascii_shift(txt,n):
+def ascii_shift(txt,n = None):
     '''shifts each char in 'txt' by 'n' places on the ascii table (only printable 32>126) (modulo)
     returns shifted string
     txt -> str
     n -> int'''
+    if n == None:
+        n = randint(0,126)
+        
     encrypted = ''
     for c in txt:
         a = ord(c)-32
@@ -34,12 +39,21 @@ def ascii_shift(txt,n):
     return encrypted
 
 # caesar shift
-def caesar_shift(txt, n):
+def caesar_shift(txt, n=None):
     '''Shifts each char in 'txt' by 'n' places on the alphabetical scale
     Conserves case
     Ignores punctuation'''
+    if n == None:
+        n = randint(0,27)
     encrypted = ''
+    for l in txt:
+        if l.lower() not in 'abcdefghijklmnopqrstuvwxyz':
+            encrypted += l
+            continue
+        encrypted += '+'
+        pass
     return encrypted
+    
 
 # reverse
 def reverse(txt):
@@ -49,8 +63,18 @@ def reverse(txt):
 # scramble (by word)
 def scramble_by_word(txt):
     '''Scrambles each word in txt'''
-
+    words = txt.split()
+    scrmbled = []
+    for w in words:
+        scrmbled.append(scramble(w))
+    return ' '.join(scrmbled)
+    
 # scramble (entire)
+def scramble(txt):
+    '''Scrambles txt as a whole'''
+    a = list(txt)
+    shuffle(a)
+    return ''.join(a)
 
 
-ciphers = [ascii_shift, caesar_shift, reverse]
+ciphers = [ascii_shift, caesar_shift, reverse, scramble_by_word, scramble]
