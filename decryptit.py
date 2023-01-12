38fafbb982ed89594fb4fe12ca5e1628adf87f82
acemavrick <116685696+acemavrick@users.noreply.github.com>
2023-01-12 16:01:53 -0600

    Update decryptit.py
    Added commands,
    Cleaned up, renovated count_diff to be more accurate

--- decryptit.py
+++ decryptit.py
@@ -14,19 +14,7 @@ quotes = [x.strip() for x in quotes]
 from random import *
 # Van Rossum, G. (2020). The Python Library Reference, release 3.8.2. Python Software Foundation.
 
-def count_diff(txta,txtb):
-    '''Counts the number of chars different between txta and txtb
-    strips both strings
-    compares the minimum length'''
-    txta, txtb = txta.strip(), txtb.strip()
-    diff = 0
-    l = min(len(txta),len(txtb))
-    for i in range(l):
-        if txta[i] != txtb[i]:
-            diff += 1
-    return diff
-
-# create functions for diff. encryptions
+### Ciphers ###
 
 # ascii shift
 def ascii_shift(txt,n = None):
@@ -96,29 +84,59 @@ def scramble(txt,n = None):
     shuffle(a)
     return ''.join(a)
 
+### End Ciphers ###
+
+def diff(txta,txtb):
+    '''Counts the number of chars different between txta and txtb
+    strips both strings
+    compares the minimum length'''
+    txta, txtb = txta.strip(), txtb.strip()
+    d = sum(1 if a != b else 0 for a, b in zip(txta,txtb))
+    return d
+
+def fttext(func):
+    '''Func to text... returns the name.
+        func == function object'''
+    rawf = str(func)
+    if rawf[1] == 'b':
+        # is builtin function
+        return rawf[rawf.find('n ')+2:-1]
+    else:
+        return rawf[rawf.find('n ')+2:rawf.find(' at')]
+
+
 def play():
-    ciphers = [ascii_shift, caesar_shift, reverse, scramble_by_word, scramble]
-    toquit = '.q'
+    # no scramble bc it's too hard
+    ciphers = [ascii_shift, caesar_shift, reverse, scramble_by_word]
+    toquit = '-q'
     instructions = \
         f'''
 Welcome to Decrypt It!
 This game improves your cryptographic and pattern recognition skills.
 You will be given a message "encrypted" in a simple "cipher".
-The "ciphers" are: Caesar Shift, Ascii Shift, Scramble (whole or by-word), and reverse.
-
+The "ciphers" are: {' '.join([fttext(x).capitalize() for x in ciphers])}.
+{input()}
 Your task is to decrypt the message, case sensitive.
 All messages are grammatically correct and they all make sense. Most of them have valid words; some don't.
 
 You have an infinite number of attempts. However, each attempt will be scored.
-Every correct submission is +1 point. Every incorrect submission is 0 points. Every skipped submission is -1 points.
+    Every correct submission is +1 point.
+    Every incorrect submission is 0 points.
+    Every skipped submission is -1 points.'
 Enter a blank line to skip.
+
+You are given use of commands to help you decrypt the message (parameters are seperated by spaces):
+    '-hint' for a hint
+    '-help' for help
+    '-q' to quit
+    '-ascii num' to use ascii shift
+    '-caesar num' to use caesar shift
+    '-reverse' to reverse
+    
 
 Note: Most messages end and begin with a non-space character. Spaces at the end and beginning of your submission will be ignored.
 
-Enter '{toquit}' to quit.
-
-Good luck!
-        
+Good luck! 
         '''
     pts = 0
     print(instructions)
@@ -130,22 +148,44 @@ Good luck!
         # remove the quote so no repetition
         quotes.remove(message)
         method = choice(ciphers)
-        encrypted = method(message)
+        encrypted = method(message, randint(-300,300))
         while message == encrypted:
             encrypted = method(message)
         print()
         print('You have',pts,'point(s).')
         print(f'{numtime}: |-|{encrypted}|-|')
+        tries = 0
         
         while True:
+            tries += 1
             # each round
-            tries = 0
             attempt = input('>: ').strip()
             print()
-            
-            if attempt == toquit:
-                break
-                
+            if attempt[0] == '-': #is a command
+                match attempt[1]:
+                    case 'q':
+                        break
+                    case 'h':
+                        match attempt[2]:
+                            case 'e':
+                                print(instructions)
+                            case 'i':
+                                l = randint(0,len(fttxt(method))-3)
+                                u = randint(l,len(fttxt(method))-1)
+                                print(fttxt(method)[l:u])
+                    case 'r':
+                        print(reverse(encrypted))
+                        
+                    case 'a' | 'c':
+                        cmd, num = attempt[1:].split(' ')
+                        num = int(num)
+                        match cmd[0]:
+                            case 'a':
+                                print(ascii_shift(encrypted,num))
+                            case 'c':
+                                print(caesar_shift(encrypted,num))
+                continue
+                        
             if message == attempt:
                 print('Correct! +1 point.')
                 pts += 1
@@ -153,15 +193,14 @@ Good luck!
 
             if attempt == '':
                 print('Skipped. -1 point.')
+                print(f'The message was "{message}".')
                 pts -= 1
                 break
 
             print('Incorrect. 0pts.')
-            numdiff = count_diff(message, attempt)
 
-            if tries%5 == 0:
+            if tries%8 == 0:
                 print('Remember, it will make sense.')
-            tries += 1
                 
         if attempt == toquit:
             print('Thank you for playing! You got',pts,'points.')
